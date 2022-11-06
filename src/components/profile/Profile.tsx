f34f6cb7522108977893ac787c57dc6cc0fb57f1
Egorych <mikhalevichegor@gmail.com>
2022-11-06 23:12:18 +0300

    14. Уроки React JS (css-модули, css-modules)Profile className s.item

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,33 +1,35 @@
 import React from "react";
-import "./Profile.css"
+import s from "./Profile.module.css"
 
 
-export function Profile () {
+export function Profile() {
     return (
-            <div className="content">
+        <div className="content">
+            <div>
+                <img
+                    src="https://commercialisation.esa.int/wp-content/uploads/2021/11/earth-g1f2e40a34_1280-1280x400.jpg"
+                    alt="img"/>
+            </div>
+            <div>
+                Ava + description
+            </div>
+            <div>
+                My Post
                 <div>
-                    <img
-                        src="https://commercialisation.esa.int/wp-content/uploads/2021/11/earth-g1f2e40a34_1280-1280x400.jpg"
-                        alt="img"/>
+                    New post
                 </div>
-                <div>
-                    Ava + description
-                </div>
-                <div>
-                    My Post
-                    <div>
-                        New post
-                    </div>
-                    <div>
+                <div className={s.post}>
+                    <div className={s.item}>
                         Post 1
                     </div>
-                    <div>
+                    <div className={s.item}>
                         Post 2
                     </div>
-                    <div>
+                    <div className={s.item}>
                         Post 3
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
