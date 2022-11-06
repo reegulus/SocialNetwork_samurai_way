912a41f691ca9220c2b92bea3f8c95030996de24
Egorych <mikhalevichegor@gmail.com>
2022-11-06 22:00:30 +0300

    12. Уроки React JS (Разбивка на компоненты)/ Profile.tsx

---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Profile.css"
+
+
+export function Profile () {
+    return (
+            <div className="content">
+                <div>
+                    <img
+                        src="https://commercialisation.esa.int/wp-content/uploads/2021/11/earth-g1f2e40a34_1280-1280x400.jpg"
+                        alt="img"/>
+                </div>
+                <div>
+                    Ava + description
+                </div>
+                <div>
+                    My Post
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                    <div>
+                        Post 3
+                    </div>
+                </div>
+            </div>
+    )
+}
