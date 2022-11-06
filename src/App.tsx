d67bdea8dc957f87726912cf97ef621c0a0aca31
Egorych <mikhalevichegor@gmail.com>
2022-11-06 23:17:17 +0300

    14. Уроки React JS (css-модули, css-modules)App.tsx changed name in Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/header/Header";
-import {NavBar} from "./components/navBar/NavBar";
+import {Navbar} from "./components/navBar/Navbar";
 import {Profile} from "./components/profile/Profile";
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <NavBar/>
+            <Navbar/>
             <Profile/>
         </div>
     );
