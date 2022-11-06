07b9e542a86af1027bfb8f6b0e64b2531e44f446
Egorych <mikhalevichegor@gmail.com>
2022-11-06 19:44:02 +0300

    11 less. created elements header, nav, content

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className = "header">
+                <img src="https://avatars.githubusercontent.com/u/19506676?v=4" alt="img"/>
+            </header>
+            <nav className= "navBar">
+                <div>Profile</div>
+                <div>Message</div>
+            </nav>
+            <div className = "content">
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;
