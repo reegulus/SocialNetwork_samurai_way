1276e5f568fffb360755cc66688e4766a1adcee6
Egorych <mikhalevichegor@gmail.com>
2022-11-06 23:15:40 +0300

    14. Уроки React JS (css-модули, css-modules)Header.tsx added className item

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Header.css"
+import s from "./Header.module.css"
 
 export function Header () {
     return (
-        <div className="header">
+        <div className={s.header}>
                 <img src="https://files.codingninjas.com/profile98225580ab82cdabc7b221549564635ee6b84c.png" alt="img"/>
                 <img
                     src="https://res.cloudinary.com/practicaldev/image/fetch/s--xqQ332cg--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/ltnifh5z7pkeeb2jzt5p.png"
