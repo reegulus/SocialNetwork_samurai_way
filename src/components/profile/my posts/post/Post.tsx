f60f445cac83e9898b2f6dfe94a64575bb454234
Egorych <mikhalevichegor@gmail.com>
2022-11-07 16:13:30 +0300

    15. Уроки React JS (структура папок, новые компоненты, улучшаем css) thrown props: likesCount in component Post

--- src/components/profile/my posts/post/Post.tsx
+++ src/components/profile/my posts/post/Post.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import s from "./Post.module.css"
 
+type PostPropsType = {
+    message: string
+    likesCount: number
+}
 
-export function Post() {
+export function Post(props: PostPropsType) {
     return (
         <div className={s.post}>
             <div className={s.item}>
                 <img
                     src="https://img.freepik.com/free-vector/cute-cat-ninja-holding-sword-cartoon-vector-icon-illustration-animal-holiday-icon-concept-isolated_138676-6049.jpg"
                     alt="icons"/>
-                Post 1
+                {props.message}
+                <div>likes: {props.likesCount}</div>
             </div>
         </div>
     )
