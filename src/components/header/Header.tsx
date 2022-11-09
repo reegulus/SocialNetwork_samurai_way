1ad8c5323f92808e622b1e311523b2be790e5385
Egorych <mikhalevichegor@gmail.com>
2022-11-09 19:40:15 +0300

    styles header and img

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,9 +5,9 @@ export function Header () {
     return (
         <div className={s.header}>
                 <img src="https://files.codingninjas.com/profile98225580ab82cdabc7b221549564635ee6b84c.png" alt="img"/>
-                <img
+                {/*<img
                     src="https://res.cloudinary.com/practicaldev/image/fetch/s--xqQ332cg--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/ltnifh5z7pkeeb2jzt5p.png"
-                    alt="img"/>
+                    alt="img"/>*/}
         </div>
     )
 }
