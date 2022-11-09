ab13b19f1bb1a54b5afd87950a19e8d73cbd1943
Egorych <mikhalevichegor@gmail.com>
2022-11-09 18:34:17 +0300

    New component Dialogs.tsx and threw the component into APP

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import './App.css';
 import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navBar/Navbar";
 import {Profile} from "./components/profile/Profile";
+import {Dialogs} from "./components/dialogs/Dialogs";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Navbar/>
+            <div>
+                <Dialogs/>
             <Profile/>
+            </div>
         </div>
     );
 }
--- src/components/profile/my posts/MyPosts.tsx
+++ src/components/profile/my posts/MyPosts.tsx
@@ -16,7 +16,6 @@ export function MyPosts() {
                 <div>
                    <Post message={"Hi, samurai!"} likesCount={5}/>
                    <Post message={"I am an incubator student."} likesCount={7}/>
-                   <Post message={"Is today the first lesson on Monday?"}likesCount={3}/>
                 </div>
             </div>
     )
