a1151c7c599ac0b0ebd9c16efd81e7b703808b27
Egorych <mikhalevichegor@gmail.com>
2022-11-14 01:16:40 +0300

    23. Уроки React JS (props для Диалогов) create new component

---
+++ src/components/profile/profileInfo/ProfileInfo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from "./ProfileInfo.module.css"
+
+export function ProfileInfo() {
+    return (
+        <div>
+            <div className={s.profile_info}>
+                <img
+                    src="https://lookingforclan.com/sites/default/files/styles/header_background/public/2017-10/samurai-1032044.jpg?itok=AgXodXzd"
+                    alt=""/>
+            </div>
+            <div className={s.avatDes}>
+                Ava + description
+            </div>
+        </div>
+    )
+}
