8cae85d34261f40bb59d71be1bf6790599dd8c50
Egorych <mikhalevichegor@gmail.com>
2022-11-14 01:15:05 +0300

    23. Уроки React JS (props для Диалогов) refactor component

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import s from "./Dialogs.module.css"
 import {Message} from "./message/Message";
-import {Dialog} from "./dialog/Dialog";
+import {DialogItem} from "./dialog/DialogItem";
 
 export function Dialogs() {
     return (
         <div className={s.dialogs}>
-            <Dialog/>
-            <Message/>
+            <div className={s.dialogs_items}>
+                <DialogItem id={1} name={"Peter Quill"}/>
+                <DialogItem id={2} name={"Gamora"}/>
+                <DialogItem id={3} name={"Groot"}/>
+                <DialogItem id={4} name={"Drax Destroyer"}/>
+                <DialogItem id={5} name={"Raccoon Rocket"}/>
+            </div>
+            <div className={s.messages_items}>
+                <Message
+                    message={"- It's still flowers! In the ultraviolet, there is generally a masterpiece of abstract art!"}/>
+                <Message message={"- Yes, Quil. On the ship is a pigsty."}/>
+                <Message message={"- I am Groot!"}/>
+                <Message message={"- I'm going to drink"}/>
+                <Message message={"- So, who is here for the first time in space?"}/>
+            </div>
         </div>
     )
 }
