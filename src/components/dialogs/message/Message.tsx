b3ad81b89b5fe2ea9c8bd7a0656637c2ac7ee170
Egorych <mikhalevichegor@gmail.com>
2022-11-14 01:15:36 +0300

    23. Уроки React JS (props для Диалогов) refactor component props

--- src/components/dialogs/message/Message.tsx
+++ src/components/dialogs/message/Message.tsx
@@ -1,20 +1,14 @@
 import React from "react";
-import s from "../Dialogs.module.css";
+import s from "./Message.module.css";
 
-export function Message() {
+type MessagePropsType = {
+    message: string
+}
+
+export function Message(props: MessagePropsType) {
     return (
         <div className={s.messages_items}>
-            <div className={s.message}>- It's still flowers! In the ultraviolet, there is generally a masterpiece of
-                abstract art!
-            </div>
-            <div className={s.message + ' ' + s.active}>- Yes, Quil. On the ship is a pigsty.
-            </div>
-            <div className={s.message}> - I am Groot!
-            </div>
-            <div className={s.message}> - I'm going to drink
-            </div>
-            <div className={s.message}> - So, who is here for the first time in space?
-            </div>
+            <div className={`${s.message} $${s.active}`}>{props.message}</div>
         </div>
     )
 }
