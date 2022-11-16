48df338278598e682aea181411133f2f14d96acd
Egorych <mikhalevichegor@gmail.com>
2022-11-16 20:29:40 +0300

    24. Уроки React JS (UI - BLL, выносим данные в сторону)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,9 @@ import s from "./Header.module.css"
 export function Header () {
     return (
         <div className={s.header}>
-                <img src="https://files.codingninjas.com/profile98225580ab82cdabc7b221549564635ee6b84c.png" alt="img"/>
+            <img src="https://images.fineartamerica.com/images/artworkimages/medium/3/programmer-coding-samurai-japan-debugging-funny-lisa-stronzi-transparent.png" alt=""/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJsx06OuetR_F6D5HO9Wd24d0UuPpoR9ffng&usqp=CAU" alt=""/>
+               {/* <img src="https://files.codingninjas.com/profile98225580ab82cdabc7b221549564635ee6b84c.png" alt="img"/>*/}
                 {/*<img
                     src="https://res.cloudinary.com/practicaldev/image/fetch/s--xqQ332cg--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/ltnifh5z7pkeeb2jzt5p.png"
                     alt="img"/>*/}
--- src/components/profile/profileInfo/ProfileInfo.tsx
+++ src/components/profile/profileInfo/ProfileInfo.tsx
@@ -5,6 +5,7 @@ export function ProfileInfo() {
     return (
         <div>
             <div className={s.profile_info}>
+
                 <img
                     src="https://lookingforclan.com/sites/default/files/styles/header_background/public/2017-10/samurai-1032044.jpg?itok=AgXodXzd"
                     alt=""/>
