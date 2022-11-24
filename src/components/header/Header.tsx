2048f4fef82e140ca9d7d740c7dd3a0c4f80d3dd
Egorych <mikhalevichegor@gmail.com>
2022-11-24 23:07:44 +0300

    26. Уроки React JS (структура проекта, ещё раз про ui - bll) img - refactor

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import s from "./Header.module.css"
-import code_samurai from "./code_samurai-PhotoRoom-PhotoRoom.png"
+import code_samurai from "../../img/code_samurai_logo.png"
 
 export function Header () {
     return (
         <div className={s.header}>
-            <img src={code_samurai} alt=""/>
-            {/*<img src="https://images.fineartamerica.com/images/artworkimages/medium/3/programmer-coding-samurai-japan-debugging-funny-lisa-stronzi-transparent.png" alt=""/>*/}
-            {/*<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJsx06OuetR_F6D5HO9Wd24d0UuPpoR9ffng&usqp=CAU" alt=""/>*/}
-               {/* <img src="https://files.codingninjas.com/profile98225580ab82cdabc7b221549564635ee6b84c.png" alt="img"/>*/}
-                {/*<img
-                    src="https://res.cloudinary.com/practicaldev/image/fetch/s--xqQ332cg--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/ltnifh5z7pkeeb2jzt5p.png"
-                    alt="img"/>*/}
+            <img src={code_samurai} alt="logo"/>
         </div>
     )
 }
