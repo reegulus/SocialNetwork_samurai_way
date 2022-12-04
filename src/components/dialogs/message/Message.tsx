ed9237d3dd9283944478c5648d35393247e72d94
Egorych <mikhalevichegor@gmail.com>
2022-12-04 21:34:16 +0300

    31. Уроки React JS - onClick, ref, VirtualDOM

--- src/components/dialogs/message/Message.tsx
+++ src/components/dialogs/message/Message.tsx
@@ -11,9 +11,17 @@ type MessagePropsType = {
 
 export function Message(props: MessagePropsType) {
     const messageMap = props.message.map(el => <div key={el.id} className={`${s.message} ${s.active}`}>{el.message}</div>)
+
+    const newMessageElements = React.createRef<HTMLTextAreaElement>()
+    const addMessage = () => {
+        const text = newMessageElements.current?.value
+        alert(text)
+    }
     return (
         <div className={s.messages_items}>
             <div>
+                <textarea ref={newMessageElements} placeholder={'message'}></textarea>
+                <button onClick={addMessage} >add message</button>
                 {messageMap}
             </div>
         </div>
